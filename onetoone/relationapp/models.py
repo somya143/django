fb272903c94d2acc1e3e398479dee7c4ce3608df
Somya Ranjan Singh <maaheersj@gmail.com>
2025-01-03 13:37:10 +0530

    many to many and one to many relationship

--- onetoone/relationapp/models.py
+++ onetoone/relationapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+# one to one relationship
 class Author(models.Model):
     name = models.CharField(max_length=200)
 
@@ -8,3 +10,17 @@ class Contact(models.Model):
     address = models.CharField(default="Jagdalpur",max_length=300)
     phone = models.IntegerField()
     author = models.OneToOneField(Author,on_delete=models.CASCADE)
+
+# one to many relationship
+class Article(models.Model):
+    title = models.CharField(max_length=400)
+    description = models.TextField(default='No description required')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE,related_name='articles')
+
+# many to many relationship
+class Courses(models.Model):
+    title = models.CharField(max_length=400)
+
+class Students(models.Model):
+    name = models.CharField(max_length=200)
+    courses = models.ManyToManyField(Courses,related_name='students')
--- onetoone/relationapp/views.py
+++ onetoone/relationapp/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render,HttpResponse
-from .models import Author,Contact
+from .models import Author,Contact,Article,Students,Courses
 # Create your views here.
 def onetoone(request):
     # author = Author.objects.create(name="Author 1")
-    author = Author.objects.get(id=1)
+    # author = Author.objects.get(id=1)
     # Contact.objects.create(author=author, phone=85426266262)
     # contact = Contact.objects.get(id=1)
-    return HttpResponse(author.contact.phone)
+    # one to manuy
+    # Article.objects.create(author=author,title='Life of a trader', description="It is not easy")
+
+    # # many to many
+    # student1 = Students.objects.create(name='Student 1')
+    # student2 = Students.objects.create(name='Student 2')
+    # course1 = Courses.objects.create(title='Python Django course')
+    # course2 = Courses.objects.create(title='python flask course')
+    student1 = Students.objects.get(id=1)
+    student2 = Students.objects.get(id=2)
+    course1 = Courses.objects.get(id=2)
+    course2 = Courses.objects.get(id=1)
+    # course1.students.add(student1)
+    # course2.students.add(student1)
+    return HttpResponse(course2.students.all()[0].name)
